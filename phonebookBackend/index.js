77a055a200bc97786ed8fe7873057e8ea1041b51
Andres Laverde <andres.laverde@protonmail.com>
2026-08-08 23:41:33 -0700

    update: add post route

--- phonebookBackend/index.js
+++ phonebookBackend/index.js
@@ -55,6 +55,37 @@ app.delete('/api/persons/:id', (request, response) => {
     response.sendStatus(204).end();
 });
 
+const generateId = () => {
+    let newId = null;
+    do {
+        newId = Math.random() * 10000;
+    } while (persons.some(p => p.id === newId));
+    return String(newId);
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+    if (body.name && body.number) {
+        const personExists = persons.some(p => p.name === body.name);
+        if (personExists) {
+            return response.status(400).json({
+                error: 'person already exists'
+            });
+        }
+        const newEntry = {
+            name: body.name,
+            number: body.number,
+            id: generateId()
+        }
+        persons = persons.concat(newEntry);
+        response.json(newEntry);
+    } else {
+        return response.status(400).json({
+            error: 'name or number missing'
+        });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })

2ef4e85803c54fbef58e8c6e4490d4e7eeacd2d0
Andres Laverde <andres.laverde@protonmail.com>
2026-08-08 22:31:36 -0700

    update: add /info route

--- phonebookBackend/index.js
+++ phonebookBackend/index.js
@@ -4,25 +4,25 @@ const app = express();
 app.use(express.json());
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ];
 const PORT = 3001;
@@ -31,7 +31,16 @@ app.get('/api/persons', (request, response) => {
     response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+    const receivedAt = new Date();
+    const content = `
+    <h3>Phonebook has info for ${persons.length} people</h3>
+    <p>${receivedAt}</p>
+    `;
+    response.send(content);
+});
+
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 })

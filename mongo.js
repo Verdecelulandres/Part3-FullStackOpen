72d96ad3660ea9d5f2b7e4a5a430a82ed912de52
Andres Laverde <andres.laverde@protonmail.com>
2026-08-11 14:32:58 -0700

    create: mongoDB connection service file

---
+++ mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+// Not enough arguments errors.
+if (process.argv.length < 3) {
+    console.log('give password as argument');
+    process.exit(1);
+} else if (process.argv.length === 4) {
+    console.log('Please provide a name and phone number to be saved.');
+    process.exit(1);
+} else if (process.argv.length > 5) {
+    console.log('Too many arguments');
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://andreslaverde_db_user:${password}@cluster0.io7wr4m.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(url, { family: 4 });
+
+const phonebookEntrySchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+});
+
+const Entry = mongoose.model('Entry', phonebookEntrySchema);
+
+if (process.argv.length === 3) {
+
+} else if (process.argv.length === 5) {
+
+    
+}
+

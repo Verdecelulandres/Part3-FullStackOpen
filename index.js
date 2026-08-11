f4af32aa5e92ccfc08a4ef8edeed64ceceadeb4c
Andres Laverde <andres.laverde@protonmail.com>
2026-08-10 20:47:30 -0700

    create: frontend production build

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('dist'));
 morgan.token('logData', (req, res) => JSON.stringify(res.logData));
 app.use(morgan(function (tokens, req, res) {    
     return [

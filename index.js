5dd8d5f70c757ecc340082f02683d2503666c190
Andres Laverde <andres.laverde@protonmail.com>
2026-08-10 21:17:25 -0700

    update: use port as an env variable

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ let persons = [
         "number": "39-23-6423122"
     }
 ];
-const PORT = 3001;
 
 app.get('/api/persons', (request, response) => {
     response.json(persons);
@@ -101,6 +100,7 @@ app.post('/api/persons', (request, response) => {
     }
 });
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
